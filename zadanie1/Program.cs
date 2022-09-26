3a049f0bff155a68fda68471b6b6337473a79565
Uliana <Uliana@mail.ru>
2022-09-26 16:34:45 +0300

    dz3

---
+++ zadanie1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число! ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Число: {number} - палиндром");
+  }
+  else Console.WriteLine($"Число: {number} - не палиндром");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введите верное число!");
+
